e9b2611367d1442b3959ff59e1c4ab63419d9345
Jason Kim <jasonckim.24@gmail.com>
2017-02-23 14:13:51 -0800

    Checkpoint 12: completed

--- scripts/album.js
+++ scripts/album.js
@@ -32,9 +32,9 @@ var albumPicasso = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
         '<tr class="album-view-song-item">'
-    +   '   <td class="song-item-number">' + songNumber + '</td>'
-    +   '   <td class="song-item-title">' + songName + '</td>'
-    +   '   <td class="song-item-duration">' + songLength + '</td>'
+    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' +         songNumber + '</td>'
+    + '  <td class="song-item-title">' + songName + '</td>'
+    + '  <td class="song-item-duration">' + songLength + '</td>'
     +   '</tr>'
     ;
     
@@ -63,8 +63,32 @@ var createSongRow = function(songNumber, songName, songLength) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
+
+// Elements to add listeners to
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
  
- window.onload = function() {
-     setCurrentAlbum(albumPicasso);
- };
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+window.onload = function() {    
+    setCurrentAlbum(albumPicasso);
+ 
+    songListContainer.addEventListener('mouseover', function(event) {
+        // Only target individidual song rows during event delegation
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            // Change the content from the number to the play button's HTML
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    });
+    
+    for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event){
+            // Selects first child element, which is song-item-number element
+            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        });
+                                     
+    }
+};
+
+    
     
