5ae677a960a5838e0954ae3e9a54edfed19db485
Jason Kim <jasonckim.24@gmail.com>
2017-03-14 19:10:39 -0700

    Checkpoint 16 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,3 +1,37 @@
+$(window).load(function() {
+    // Convert to window to jQuery update .innerHeight to jQuery height() method
+    if ($(window).height() > 950) {
+        animatePoints();
+    }
+    
+    // Replace getBoundingClientRect() with .offset() method               
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+    // Replace addEventListener() with scroll() method
+    $(window).scroll(function(event) {
+        // Replace document.documentElement.scrollTop || document.body.scrollTop with 
+        // $(window).scrollTop() 
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
+        }
+    });
+});
+
+// Remove DOM selector that gets .point element by class name
+var animatePoints = function() {
+    var revealPoint = function() {
+        // Use css() method in place of multiple style instances
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    // Replace for loop with $.each() function
+    $.each($('.point'), revealPoint);
+};
+
+/*var pointsArray = document.getElementsByClassName('point');
+
 var animatePoints = function (points) {
     var revealPoint = function (index) {
         points[index].style.opacity = 1;
@@ -10,22 +44,4 @@ var animatePoints = function (points) {
             revealPoint(i);
         }
  
-};
-
-window.onload = function() {
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
-    }
-    
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top -            window.innerHeight + 200;
-    
-    window.addEventListener('scroll',function(event){
-        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
-        }
-    });
-}
-
-var pointsArray = document.getElementsByClassName('point');
-
+};*/
