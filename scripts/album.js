4f729353b7dc4a9f0f594b30edfb7a1a7d42de76
Jason Kim <jasonckim.24@gmail.com>
2017-04-05 19:19:52 -0700

    Checkpoint 19 completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,17 +1,4 @@
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta', duration: '2:15'}
-     ]
- };        
+  
 
 // Elements to add listeners to
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
